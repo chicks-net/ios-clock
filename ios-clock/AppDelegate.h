a16215f7acaf1a94c13625510ff1d8b3222c64f8
Christopher Hicks <chicks.net@gmail.com>
2016-05-01 13:28:22 -0700

    xcode boilerplate :gift:

---
+++ ios-clock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-clock
+//
+//  Created by Christopher Hicks on 5/1/16.
+//  Copyright © 2016 Christopher Hicks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-clock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-clock
+//
+//  Created by Christopher Hicks on 5/1/16.
+//  Copyright © 2016 Christopher Hicks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
